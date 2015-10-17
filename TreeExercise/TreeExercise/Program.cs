d847fc2bbde12e8a7b2ad9aa76fd7445a9d7515b
Morrgan Sweeney-Charlton <mxs4952@rit.edu>
2015-10-17 18:05:07 -0400

    Added a line of code at the end so the executable file would run. NOW it's finished.

--- TreeExercise/TreeExercise/Program.cs
+++ TreeExercise/TreeExercise/Program.cs
@@ -125,6 +125,8 @@ namespace TreeExercise
                     Console.WriteLine(); // line break
                 }
             }
+            Console.WriteLine("Tree complete. Press enter to close program.");
+            string closed = Console.ReadLine();
         }
     }
 }
